ed386438237a94655e96e4428d14c15d1059aa2f
Dor04ka <dor04ka@mail.ru>
2023-10-07 18:13:59 +0300

    Добавьте файлы проекта.

---
+++ lab3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+	
+	long size; // размер массива
+	cin >> size;
+	long* m = new long[size]; // сам массив
+	for (long i = 0; i < size; ++i) { m[i] = 0; }
+
+	string s1; // строка значений, которую вводим
+	cin >> s1;
+	string temp = ""; //промежуточная строка, через которую будем писать числа в массив
+	long prob = 0; // счетчик пробелов в строке чисел / номер элемента в массиве
+	
+	for (long i = 0; i < s1.length(); ++i) {
+		if (s1[i] != ' ') {
+			temp += s1[i];
+		}
+		else {
+			m[prob] = stoi(temp);
+			prob += 1;
+			temp = "";
+		}
+	}
+	for (long i = 0; i < size; ++i) {
+		cout << m[i] << " ";
+	}
+}
